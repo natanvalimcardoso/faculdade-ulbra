b533628d76bc0b151c68c50d6c8c6ae96cf91fbf
natanvalimcardoso <natanvalimcardoso@htomail.com>
2021-08-10 19:52:20 -0300

    atv1

---
+++ src/ex1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ex1 {
+    public static void main(String[] args) throws Exception {
+        
+        double soma = 0;
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("Valor do produto: ");
+        double input = s.nextInt();
+
+        Scanner n = new Scanner(System.in);
+        System.out.print("Porcentagem de desconto: ");
+        double input2 = n.nextInt();
+
+        input2 = ( input * input2  / 100 );
+        soma = (input - input2);
+        System.out.println(" O valor do produto é: " + input + " com um desconto de: "+ input2 + " ficando no total: "+soma);
+        
+    }
+}
