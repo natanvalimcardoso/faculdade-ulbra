849db94610c269822356c5752e21e7c10109f92b
Natan Valim <42617787+natanvalimcardoso@users.noreply.github.com>
2021-09-14 20:43:22 -0300

    Create java3-1.java

---
+++ src/java3-1.java
@@ -0,0 +1,16 @@
+public class exemploArrays {
+    public static void main(String[] args) {
+
+        int[] numero = new int[3];
+        String [] frutas = {"fruta","maca","fruta"};
+
+        numero[0] = 1;
+        numero[1] = 2;
+        numero[2] = 3;
+
+        System.out.println(numero.length);
+        System.out.println(frutas[1]);
+
+
+    }
+}
