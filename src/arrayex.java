8e760a3361e1d6246d5a7069fa2e3e147a6a022a
Natan Valim <42617787+natanvalimcardoso@users.noreply.github.com>
2021-09-17 18:17:46 -0300

    Create arrayex.java

---
+++ src/arrayex.java
@@ -0,0 +1,21 @@
+public class array3 {
+    public static void main(String[] args) {
+
+        Double[] numero = new Double[3];
+
+        numero[0] = 1.5;
+        numero[1] = 2.6;
+        numero[2] = 3.9;
+
+        Double[] meuArray1;
+        meuArray1 = new Double[4];
+
+        meuArray1[0] = 1.6;
+        meuArray1[1] = 1.3;
+        meuArray1[2] = 1.8;
+        meuArray1[3] = 1.4;
+
+        System.out.println(numero.length);
+        System.out.println(meuArray1.length);
+    }
+}
