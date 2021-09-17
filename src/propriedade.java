e501099d81883c48d511d8f6f188119c572c874f
Natan Valim <42617787+natanvalimcardoso@users.noreply.github.com>
2021-09-17 17:42:54 -0300

    Create propriedade.java

---
+++ src/propriedade.java
@@ -0,0 +1,10 @@
+public class array2 {
+    public static void main(String[] args) {
+
+
+        String objeto = "I like java";
+
+        System.out.println("A frase tem um total de: " + objeto.length() +" caracteres ");
+        System.out.println("A frase na posição 7 encontra a letra: " + objeto.charAt(7));
+    }
+}
