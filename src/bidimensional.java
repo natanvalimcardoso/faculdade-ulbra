300804998d1b55add368bf194410885e043f9439
Natan Valim <42617787+natanvalimcardoso@users.noreply.github.com>
2021-09-19 17:38:40 -0300

    Create bidimensional.java

---
+++ src/bidimensional.java
@@ -0,0 +1,13 @@
+public class array7 {
+    public static void main(String[] args) {
+
+        int imagem[][] = new int[40][40]; // classe bidimensional
+        for (int i = 0; i < imagem.length; i++) { // repeti a quantidade de vezes
+            for (int j = 0; j < imagem[i].length; j++) { //esse gera um numero de 1 a 40
+                int valor = 1 + (int) (Math.random() * 255); // aleatoriza
+                imagem[i][j] = valor;
+                System.out.print(imagem[i][j] + " ");
+            }
+        }
+    }
+}
