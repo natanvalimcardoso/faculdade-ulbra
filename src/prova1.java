06e164261f70555fc47d0a9c37914ab81e5f3236
Natan Valim <42617787+natanvalimcardoso@users.noreply.github.com>
2021-09-23 13:38:46 -0300

    Create prova1.java

---
+++ src/prova1.java
@@ -0,0 +1,21 @@
+package ex3;
+
+public class teste2 {
+    public static void main(String[] args) {
+
+        int[] numeros = new int[100];
+        String[] frutas = { "maça", "Pera", "Abacaxi", "Mamãoo" };
+        String busca = "Pera";
+
+        numeros[0] = 3;
+        numeros[1] = 4;
+        for (int i = 0; i < frutas.length; i++) {
+            if (busca.equals(frutas[i])) {
+
+                System.out.println(frutas[i].toLowerCase());
+                System.out.println(frutas[i].length());
+                System.out.println(frutas[i].charAt(0));
+            }
+        }
+    }
+}
