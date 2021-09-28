a278baa86ef62fe5e0326b3db48f49ed44a8a591
Natan Valim <42617787+natanvalimcardoso@users.noreply.github.com>
2021-09-28 15:08:51 -0300

    Create stringAleatoria.java

---
+++ src/stringAleatoria.java
@@ -0,0 +1,37 @@
+package metodos;
+
+import java.nio.charset.*;
+import java.util.*;
+
+class AlphaNumericStringGenerator {
+    public static void main(String[] args) {
+        int i = 8;
+        System.out.println("A senha aleatória é: " + getRandomString(i));
+    }
+
+    static String getRandomString(int i) {
+        byte[] bytearray;
+        String mystring;
+        StringBuffer thebuffer;
+
+        bytearray = new byte[256];
+        new Random().nextBytes(bytearray);
+
+        mystring = new String(bytearray, Charset.forName("UTF-8"));
+
+        // Create the StringBuffer
+        thebuffer = new StringBuffer();
+
+        for (int m = 0; m < mystring.length(); m++) {
+
+            char n = mystring.charAt(m);
+
+            if (((n >= 'A' && n <= 'Z') || (n >= '0' && n <= '9')) && (i > 0)) {
+
+                thebuffer.append(n);
+                i--;
+            }
+        }
+        return thebuffer.toString();
+    }
+}
