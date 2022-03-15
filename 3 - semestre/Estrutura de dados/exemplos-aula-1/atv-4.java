03bb1646708acf1dc534898389a57149b77bb765
Natan Valim <42617787+natanvalimcardoso@users.noreply.github.com>
2022-03-15 19:58:07 -0300

    Create atv-4.java

---
+++ 3 - semestre/Estrutura de dados/exemplos-aula-1/atv-4.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Atv-4 {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+      
+        double[] list = {13,32.1,8,2,21};
+      
+        int number = 0;
+
+        do {
+            System.out.println("0 = SAIR | 1 = MOSTRAR | 2 = INVERTIDA);
+            choice = input.nextInt();
+            
+            if(number == 1){
+                for (int i = 0; i < list.length; i++) {
+                    System.out.print( list[i] + ", ");
+                }
+            }
+            
+            if (number == 2) {
+                for (int i = list.length-1; i != -1; i--) {
+                    System.out.print(list[i]);
+                }
+                System.out.println();
+            }
+
+        } while (number != 0);
+
+    }
+}
