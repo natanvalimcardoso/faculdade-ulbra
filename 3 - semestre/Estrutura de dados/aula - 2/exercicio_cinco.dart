eb5dd3246f474803fa3c715a4bfbc9a07673d6cf
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-03-22 17:26:07 -0300

    feat: Exercicio 5 concluido

--- 3 - semestre/Estrutura de dados/aula - 2/exercicio_cinco.dart
+++ 3 - semestre/Estrutura de dados/aula - 2/exercicio_cinco.dart
@@ -1,3 +1,12 @@
+/*Crie um vetor capaz de armazenar 50 números inteiros. Em seguida faça o seu preenchimento
+ automático com os números de 101 a 150, ou seja, na posição número 0 ponha 101, na posição 1 
+ ponha o número 102, e assim sucessivamente. Em seguida, exiba os valores deste vetor.*/
+
 main(){
-  
+  List lista = [];
+
+  for(int i = 0; i < 50; i++){
+    lista.add(101 + i);
+  }
+  print(lista);
 }
