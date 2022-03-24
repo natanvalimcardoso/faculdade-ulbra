3d3f2ff25922acebc0d0f59002d64602b9734c06
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-03-24 18:08:54 -0300

    exercio-1

---
+++ 3 - semestre/Estrutura de dados/aula - 3/exercicio_um.java
@@ -0,0 +1,5 @@
+/* Faça um programa para ler uma matriz 4 por 4, contendo os valores de notas de g1, 
+e outro de g2. Ao final, o programa deve mostrar as médias dos alunos, mostrando se o aluno está aprovado,
+ em exame ou reprovado. Considere média > 7 para aprovação,7 < média >6 para exame; e < 6 como reprovado. 
+Importante deve ser validado que as notas aceitas sejam de 0 a 10.*/
+
